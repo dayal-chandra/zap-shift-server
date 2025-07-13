108887550e0cbc1ac32ab87e147b4530a10772de
dayal chandra <dayalchandra511@gmail.com>
2025-07-13 13:55:17 +0600

    post api created for new parcel

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
       res.send(parcels);
     });
 
+    app.post("/parcels", async (req, res) => {
+      const newParcel = req.body;
+      const result = await parcelCollection.insertOne(newParcel);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
